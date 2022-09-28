7d87e7e85ab42537ee2b72502828a7614aa16c33
adithyan98 <adithyanarayanant@gmail.com>
2022-09-28 15:01:34 +0530

    sugarCrushServer

---
+++ src/data.ts
@@ -0,0 +1,96 @@
+export const sampleFood: any[] = [
+    {
+        id:"1",
+        name:"Chocolate Cake",
+        price:400,
+        tags:['Chocolate','cake'],
+        favorite:false,
+        stars:4,
+        imageUrl:'https://i.postimg.cc/yYJFmxcT/cc1.jpg',
+        origins:['italy'],
+        cookTime:'40-50'
+    },
+    {
+        id:"2",
+        name:"Masala Cake",
+        price:400,
+        tags:['Masala'],
+        favorite:false,
+        stars:2,
+        imageUrl:'https://i.postimg.cc/yYJFmxcT/cc1.jpg',
+        origins:['italy'],
+        cookTime:'40-50'
+    },
+    {
+        id:"3",
+        name:"Watermelon Cake",
+        price:400,
+        tags:['Watermelon'],
+        favorite:false,
+        stars:4,
+        imageUrl:'https://i.postimg.cc/yYJFmxcT/cc1.jpg',
+        origins:['italy'],
+        cookTime:'40-50'
+    },
+    {
+        id:"5",
+        name:"Red Velvet",
+        price:400,
+        tags:['Red','Velvet'],
+        favorite:false,
+        stars:4,
+        imageUrl:'https://i.postimg.cc/yYJFmxcT/cc1.jpg',
+        origins:['italy'],
+        cookTime:'40-50'
+    },
+    {
+        id:"6",
+        name:"Black Forest",
+        price:400,
+        tags:['Black','Forest'],
+        favorite:false,
+        stars:4,
+        imageUrl:'https://i.postimg.cc/yYJFmxcT/cc1.jpg',
+        origins:['italy'],
+        cookTime:'40-50'
+    },
+    {
+        id:"3",
+        name:"White Forest",
+        price:400,
+        tags:['White','Forest'],
+        favorite:false,
+        stars:4,
+        imageUrl:'https://i.postimg.cc/yYJFmxcT/cc1.jpg',
+        origins:['italy'],
+        cookTime:'40-50'
+    }
+    
+]
+
+export const sampleTags:any[] = [
+    {name:'All', count:6},
+    {name:'Chocolate', count:1},
+    {name:'Masala', count:1},
+    {name:'Watermelon', count:1},
+    {name:'Velvet', count:1},
+    {name:'White', count:1},
+    {name:'Cake', count:4}
+]
+
+export const sample_users: any[] = [
+    {
+      name: "John Doe",
+      email: "john@gmail.com",
+      password: "12345",
+      address: "Toronto On",
+      isAdmin: true,
+    },
+    {
+      name: "Jane Doe",
+      email: "Jane@gmail.com",
+      password: "12345",
+      address: "Shanghai",
+      isAdmin: false,
+    },
+  ]
